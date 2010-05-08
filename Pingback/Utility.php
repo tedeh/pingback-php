893e455f7d8f92aebdf08edcc26302f62b5a96b6
tedeh <mail@tedeh.net>
2010-05-08 14:18:10 +0200

    Simplifications of Pingback_Utility

--- Pingback/Utility.php
+++ Pingback/Utility.php
@@ -1,49 +1,76 @@
 <?php
 
+if(!function_exists('http_parse_headers'))
+{
+    function http_parse_headers( $header )
+    {
+        $retVal = array();
+        $fields = explode("\r\n", preg_replace('/\x0D\x0A[\x09\x20]+/', ' ', $header));
+        foreach( $fields as $field ) {
+            if( preg_match('/([^:]+): (.+)/m', $field, $match) ) {
+                $match[1] = preg_replace('/(?<=^|[\x09\x20\x2D])./e', 'strtoupper("\0")', strtolower(trim($match[1])));
+                if( isset($retVal[$match[1]]) ) {
+                    $retVal[$match[1]] = array($retVal[$match[1]], $match[2]);
+                } else {
+                    $retVal[$match[1]] = trim($match[2]);
+                }
+            }
+        }
+        return $retVal;
+    }
+}
+
 class Pingback_Utility
 {
-  const URL = '~^(https?|ftps?)://(([a-z0-9-]+\.)+[a-z]{2,6}|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(:[0-9]+)?(/?|/\S+)$~ix';
-
-  const PINGBACK_LINK = '<link rel="pingback" href="([^"]+)" ?/?>';
+  const REGEXP_PINGBACK_LINK = '<link rel="pingback" href="([^"]+)" ?/?>';
 
   public static function isURL($url)
   {
-    return preg_match(self::URL, $url) ? true : false;
+    return filter_var($url, FILTER_VALIDATE_URL);
   }
 
   public static function isPingbackEnabled($url)
   {
-    return self::getPingbackURL($url) ? true : false;
+    return (bool)self::getPingbackServerURL($url);
+  }
+  
+  public static function getRawPostData()
+  {
+    return file_get_contents('php://input');
   }
 
-  public static function getPingbackURL($url)
+  public static function getPingbackServerURL($url)
   {
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_HEADER, true);
     curl_setopt($curl, CURLOPT_NOBODY, true);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $headers = self::parse_headers(curl_exec($curl));
+    $headers = http_parse_headers(curl_exec($curl));
     curl_close($curl);
 
-    if(isset($headers['X-Pingback']))
+    if(isset($headers['X-Pingback']) && !empty($headers['X-Pingback']))
     {
       return $headers['X-Pingback'];
     }
 
     $response = file_get_contents($url);
 
-    return preg_match(self::PINGBACK_LINK, $response, $match) ? $match[1] : false;
+    return preg_match(self::REGEXP_PINGBACK_LINK, $response, $match) ? $match[1] : false;
   }
 
   public static function isBacklinking($url, $to)
   {
-    $doc = new DOMDocument();
-    $doc->loadHTML(file_get_contents($url));
-    foreach($doc->getElementsByTagName('a') as $link)
+    $content = file_get_contents($url);
+    if($content !== false)
     {
-      if($link->getAttribute('href') == $to)
+      $doc = new DOMDocument();
+      $doc->loadHTML($content);
+      foreach($doc->getElementsByTagName('a') as $link)
       {
-        return true;
+        if($link->getAttribute('href') == $to)
+        {
+          return true;
+        }
       }
     }
     return false;
@@ -59,23 +86,6 @@ class Pingback_Utility
     $response = curl_exec($curl);
     curl_close($curl);
     return $response;
-  }
-
-  private static function parse_headers($header)
-  {
-    $retVal = array();
-    $fields = explode("\r\n", preg_replace('/\x0D\x0A[\x09\x20]+/', ' ', $header));
-    foreach( $fields as $field ) {
-      if( preg_match('/([^:]+): (.+)/m', $field, $match) ) {
-        $match[1] = preg_replace('/(?<=^|[\x09\x20\x2D])./e', 'strtoupper("\0")', strtolower(trim($match[1])));
-        if( isset($retVal[$match[1]]) ) {
-          $retVal[$match[1]] = array($retVal[$match[1]], $match[2]);
-        } else {
-          $retVal[$match[1]] = trim($match[2]);
-        }
-      }
-    }
-    return $retVal;
   }
 }
 
