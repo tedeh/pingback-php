6408cab29df14d5d90e419b1f76e7e24f1f3ac61
Tedde Lundgren <mail@tedeh.net>
2012-02-10 14:39:39 +0100

    Fix formatting  and add sensible gitignore

--- Pingback/Server.php
+++ Pingback/Server.php
@@ -1,11 +1,10 @@
 <?php
 
-require_once dirname(__FILE__).'/Exception.php';
+require_once dirname(__FILE__) . '/Exception.php';
 
-require_once dirname(__FILE__).'/Utility.php';
+require_once dirname(__FILE__) . '/Utility.php';
 
-class Pingback_Server
-{
+class Pingback_Server {
   const RESPONSE_SUCCESS                    = -1;
   const RESPONSE_FAULT_GENERIC              = 0;
   const RESPONSE_FAULT_SOURCE               = 0x0010;
@@ -40,139 +39,110 @@ class Pingback_Server
     'encoding' => 'utf-8'
   );
 
-  public function __construct($options = array())
-  {
+  public function __construct($options = array()) {
     $this->_server = xmlrpc_server_create();
-    
     $this->setOptions($options);
-    
-    if(!xmlrpc_server_register_method($this->_server, 'pingback.ping', array($this, '_ping')))
-    {
+    if(!xmlrpc_server_register_method($this->_server, 'pingback.ping', array($this, '_ping'))) {
       throw new Pingback_Exception('Failed to register method to server');
     }
   }
   
-  public function __destruct()
-  {
+  public function __destruct() {
     xmlrpc_server_destroy($this->_server);
   }
 
-  protected function _ping($method, $parameters)
-  {
+  protected function _ping($method, $parameters) {
     list($this->_requestSource, $this->_requestTarget) = $parameters;
     
-    $fault = false;
+    $fault = null;
 
-    if(!Pingback_Utility::isURL($this->_requestSource)) $fault = self::RESPONSE_FAULT_SOURCE;
+    // is the source argument really an url?
+    if(!$fault && Pingback_Utility::isURL($this->_requestSource)) $fault = self::RESPONSE_FAULT_SOURCE;
 
-    if(!Pingback_Utility::isURL($this->_requestTarget)) $fault = self::RESPONSE_FAULT_TARGET;
+    // is the target argument really an url?
+    if(!$fault && Pingback_Utility::isURL($this->_requestTarget)) $fault = self::RESPONSE_FAULT_TARGET;
 
-    if(!Pingback_Utility::isPingbackEnabled($this->_requestTarget)) $fault = self::RESPONSE_FAULT_TARGET_INVALID;
+    // is the target url pingback enabled?
+    if(!$fault && Pingback_Utility::isPingbackEnabled($this->_requestTarget)) $fault = self::RESPONSE_FAULT_TARGET_INVALID;
 
-    if(!Pingback_Utility::isBacklinking($this->_requestSource, $this->_requestTarget)) $fault = self::RESPONSE_FAULT_SOURCE_LINK;
+    // is the source backlinking to the target?
+    if(!$fault && Pingback_Utility::isBacklinking($this->_requestSource, $this->_requestTarget)) $fault = self::RESPONSE_FAULT_SOURCE_LINK;
     
-    if($fault !== false)
-    {
+    if($fault !== null) {
       $this->setFault($fault);
       return $this->getFaultAsArray($fault);
-    }
-    else
-    {
+    } else {
       $this->setSuccess();
       return $this->getSuccessAsArray();
     }
   }
   
-  public function getOption($option)
-  {
+  public function getOption($option) {
     return isset($this->_options[$option]) ? $this->_options[$option] : null;
   }
   
-  public function setOption($option, $value)
-  {
+  public function setOption($option, $value) {
     $this->_options[$option] = $value;
   }
   
-  public function setOptions($options = array())
-  {
-    foreach($options as $option => $value)
-    {
+  public function setOptions($options = array()) {
+    foreach($options as $option => $value) {
       $this->setOption($option, $value);
     }
   }
 
-  public function execute($request = null)
-  {
-    if($request)
-    {
+  public function execute($request = null) {
+    if($request) {
       $this->_request = $request;
     }
     
     $this->_response = xmlrpc_server_call_method($this->_server, $this->_request, null, array('encoding' => $this->getOption('encoding')));
   }
 
-  public function setResponse($response)
-  {
+  public function setResponse($response) {
     $this->_response = $response;
   }
 
-  public function setRequest($request)
-  {
+  public function setRequest($request) {
     $this->_request = $request;
   }
 
-  public function getRequest()
-  {
+  public function getRequest() {
     return $this->_request;
   }
 
-  public function getResponse()
-  {
+  public function getResponse() {
     return $this->_response;
   }
 
-  public function getSourceURL()
-  {
+  public function getSourceURL() {
     return $this->_sourceURL;
   }
 
-  public function getTargetURL()
-  {
+  public function getTargetURL() {
     return $this->_targetURL;
   }
   
-  public function getFaultAsArray($faultCode)
-  {
-    return array('faultCode' => $faultCode, 'faultString' => $this->responses[$faultCode]);
+  public function getFaultAsArray($faultCode) {
+    return array(
+      'faultCode' => $faultCode,
+      'faultString' => $this->responses[$faultCode]
+    );
   }
   
-  public function getSuccessAsArray()
-  {
+  public function getSuccessAsArray() {
     return array($this->responses[self::RESPONSE_SUCCESS]);
   }
 
-  public function setFault($faultCode)
-  {
+  public function setFault($faultCode) {
     $this->_response = xmlrpc_encode($this->getFaultAsArray($faultCode));
   }
   
-  public function setSuccess()
-  {
+  public function setSuccess() {
     $this->_response = xmlrpc_encode($this->getSuccessAsArray());
   }
 
-  public function isValid()
-  {
+  public function isValid() {
     return !xmlrpc_is_fault($this->_response);
   }
 }
-
-
-
-
-
-
-
-
-
-
